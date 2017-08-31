ab4dac22f417df8521019b2320d8e7ac1e553e18
Qian Yu <qyhoperose@gmail.com>
2017-08-31 15:34:18 -0700

    something

---
+++ meeting3/tip_calculator_Yu.py
@@ -0,0 +1,8 @@
+price = input("Enter the price of a meal: ")
+
+price = float(price)
+tip = price * 0.16
+total = price + tip
+
+print("A 16% tip would be ", tip)
+print("The total including tip would be ", total)
